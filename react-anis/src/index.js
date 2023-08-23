a44808101bdadac2b11ac6824aebefbedbb57289
dipu157 <dipu157@gmail.com>
2023-08-23 23:40:02 +0600

    jsx and js expe

---
+++ react-anis/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <div>
+        <h1>To Do App</h1>
+        <h3>Call Family</h3>
+    </div>
+    
+);
