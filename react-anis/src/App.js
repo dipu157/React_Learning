d803689398bab4c37c78f9c7afb728f1e743b309
dipu157 <dipu157@gmail.com>
2023-08-25 12:00:20 +0600

    multi map function usage

--- react-anis/src/App.js
+++ react-anis/src/App.js
@@ -1,12 +1,59 @@
 import React from 'react';
-import Card from './components/Card';
-import Data from './data.json'
-import { List } from './components/UniqueList/List';
+import { v4 as uuidv4 } from 'uuid';
+
+const users = [
+    {
+        id: uuidv4(),
+        name : "Manik Chan",
+        address : "This is address1",
+        phones : [
+            { home: "01213212"},
+            { office: "123456"}
+        ]
+
+    },
+    {
+        id: uuidv4(),
+        name : "Rakibul Islam",
+        address : "This is address2",
+        phones : [
+            { home: "56165251"},
+            { office: "879874854"}
+        ]
+    },
+    {
+        id: uuidv4(),
+        name :"Fatema Tuzzohra",
+        address : "This is address3",
+        phones : [
+            { home: "7897489212"},
+            { office: "564654"}
+        ]
+    }
+
+]
 
 function App() {
 
     return <div>        
-        <List />
+        <h1>Nested Lists</h1>        
+
+        {users.map((user) =>{
+            const {id, name, address, phones} = user;
+            return <div key={id}>
+                <h2>Name : {name}</h2>
+                <p>Address : {address}</p>
+                <h4>
+                    {phones.map((phone) =>{
+                        return <div>
+                            Home : {phone.home}
+                            Office :{phone.office}
+                        </div>
+                    })}
+                </h4>
+                <hr />
+            </div>
+        })}
     </div>
 }
 
