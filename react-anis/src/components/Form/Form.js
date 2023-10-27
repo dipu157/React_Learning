d78e44ee7f612de578d32db3054de9789e740b63
dipu157 <dipu157@gmail.com>
2023-10-27 23:54:03 +0600

    useState hook in Form

--- react-anis/src/components/Form/Form.js
+++ react-anis/src/components/Form/Form.js
@@ -4,30 +4,33 @@ import style from './form.module.css';
 
 export default function Form() {
 
-    const [name,setName] = useState("");
-    const [email,setEmail] = useState("");
-    const [password,setPassword] = useState("");
+    // const [name,setName] = useState("");
+    // const [email,setEmail] = useState("");
+    // const [password,setPassword] = useState("");
 
-    const hnameChange = (e) => {
-        setName(e.target.value);
-    }
+    const [user,setUser] = useState({name: "",email: "",password: ""});
+    const {name,email,password} = user;
 
-    const hemailChange = (e) => {
-        setEmail(e.target.value);
-    }
+    // const hnameChange = (e) => {
+    //     setUser({name:e.target.value, email, password});
+    // }
 
-    const hpasswordChange = (e) => {
-        setPassword(e.target.value);
+    // const hemailChange = (e) => {
+    //     setUser({name, email:e.target.value, password});
+    // }
+
+    // const hpasswordChange = (e) => {
+    //     setUser({name, email, password:e.target.value});
+    // }
+
+    const hChange = (e) => {
+        const fieldname = e.target.name;
+        setUser({...user,[e.target.name]:e.target.value});
     }
 
     const hSubmit = (e) => {
-        console.log("Form sumbitted!");        
-        let userinfo = {
-            name,
-            email,
-            password
-        }
-        console.log(userinfo);
+        console.log("Form sumbitted!");
+        console.log(user);
         e.preventDefault();
     }
 
@@ -39,17 +42,17 @@ export default function Form() {
 
             <div className={style.formGroup}>
                 <label>Name: </label>
-                <input type="text" name="name" id='name' value={name} onChange={hnameChange} required />
+                <input type="text" name="name" id='name' value={name} onChange={hChange} required />
             </div>
 
             <div className={style.formGroup}>
                 <label>Email: </label>
-                <input type="text" name="email" id='email' value={email} onChange={hemailChange} required />
+                <input type="text" name="email" id='email' value={email} onChange={hChange} required />
             </div>
 
             <div className={style.formGroup}>
                 <label>Password: </label>
-                <input type="password" name="password" id='password' value={password} onChange={hpasswordChange} required />
+                <input type="password" name="password" id='password' value={password} onChange={hChange} required />
             </div>
 
             <button type="submit">Submit</button>
